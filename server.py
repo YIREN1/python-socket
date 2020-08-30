f1a6eaf20c65290360931584e74127f66b92fcc3
Yi Ren <jamesren1998@gmail.com>
2020-08-30 10:41:53 +0800

    :tada: init

---
+++ server.py
@@ -0,0 +1,45 @@
+import socket
+import threading
+
+HEADER = 64
+PORT = 5151
+SERVER = '36.153.84.17' 
+# print(socket.gethostname())
+# SERVER = socket.gethostbyname(socket.gethostname())
+# print(SERVER)
+
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+            print(f"[{addr}] {msg}")
+            conn.send("Msg received".encode(FORMAT))
+            print(len(b"Msg received".encode(FORMAT)))
+    conn.close()        
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}:{PORT}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+
+print("[STARTING] server is starting...")
+start()    
